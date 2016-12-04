ce23809be5ba2772760b0231d518ddb294eab0ff
Andrew McCluskey <andrew@ajmccluskey.com>
2016-12-04 22:04:46 +1000

    Add syntax highlighting

--- site.hs
+++ site.hs
@@ -7,7 +7,7 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match ("images/*" .||. "js/**") $ do
         route   idRoute
         compile copyFileCompiler
 
